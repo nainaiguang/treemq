43a9ee2df554efdf231409cdd0053c15aa699209
qiunaiguang <434419565@qq.com>
2020-09-18 16:27:49 +0800

    首次上传初始项目

---
+++ src/main/java/com/tree/treemq/TreeMqApplication.java
@@ -0,0 +1,13 @@
+package com.tree.treemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tree/treemq/TreeMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tree.treemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
